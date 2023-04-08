3dcdccc384ea46213119651cf0ca4ebadea79999
Michael Coxeter <mcoxeter@gamil.com>
2023-04-08 11:01:09 +0200

    Initial checkin

---
+++ pages/api/workouts.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IWorkouts>
+) {
+  const data = require('./workouts.json');
+  res.status(200).json(data);
+}
+
+export interface IWorkouts {
+  workouts: IWorkout[];
+}
+
+export interface IWorkout {
+  name: string;
+
+  startMessages: string[];
+  endMessages: string[];
+  equipment: string[];
+
+  groups: IExerciseGroup[];
+}
+
+export interface IExerciseGroup {
+  name: string;
+  rounds: number;
+  restBetweenRounds: number;
+  exercises: IExercise[];
+}
+
+export interface IExercise {
+  name: string;
+  rest: number;
+  duration: number;
+  equipment: string[];
+}
